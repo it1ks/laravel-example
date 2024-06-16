e4186f13d0939ddaf1fc53a50a65af6ec3f31b34
it1ks <officialitiak@gmail.com>
2024-06-16 08:43:25 +0700

    first commit

---
+++ resources/views/layouts/frontend/footer.blade.php
@@ -0,0 +1,19 @@
+<footer class="footer-area">
+    <!-- Top Footer Area -->
+    <div class="top-footer-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <!-- Footer Logo -->
+                    <div class="footer-logo">
+                        <a href="/"><h3 class="text-light">LARASCHOOL</h3></a>
+                    </div>
+                    <!-- Copywrite -->
+                    <p><a href="#"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved.
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
